fd795cbae365eb8da92a58137b71adfc36c74091
Paulo Alvarez <pauloalvarez21@gmail.com>
2022-03-24 11:13:19 -0500

    primera pantalla

--- app/src/main/java/com/gaelectronica/infotrack_prueba/MainActivity.kt
+++ app/src/main/java/com/gaelectronica/infotrack_prueba/MainActivity.kt
@@ -2,10 +2,77 @@ package com.gaelectronica.infotrack_prueba
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import com.gaelectronica.infotrack_prueba.adapter.UserListAdapter
+import com.gaelectronica.infotrack_prueba.model.User
+import org.json.JSONArray
+import org.json.JSONException
 
 class MainActivity : AppCompatActivity() {
+
+    var volleyRequest: RequestQueue? = null
+    var arrayList: ArrayList<User>? = null
+    var userAdater: UserListAdapter? = null
+    var layoutManayer: RecyclerView.LayoutManager? = null
+    var reciclerUser: RecyclerView? = null
+
+    var stringLink = "https://jsonplaceholder.typicode.com/users"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        reciclerUser = findViewById<RecyclerView>(R.id.rvUser)
+        arrayList = ArrayList<User>()
+        volleyRequest = Volley.newRequestQueue(this)
+
+        getUsersArray(stringLink)
+    }
+
+    private fun getUsersArray(url: String) {
+        val jsonArrayReq = JsonArrayRequest(Request.Method.GET, url,
+                            Response.Listener {
+                                response: JSONArray ->
+                                try {
+                                    Log.d("======>", response.toString())
+
+                                    for (i in 0..response.length() -1) {
+                                        val userObj = response.getJSONObject(i)
+
+                                        var showName = userObj.getString("name")
+                                        var showUserName = userObj.getString("username")
+                                        var showEmail = userObj.getString("email")
+
+                                        var user = User()
+
+                                        user.name = showName
+                                        user.username = showUserName
+                                        user.email = showEmail
+
+                                        arrayList!!.add(user)
+
+                                        userAdater = UserListAdapter(arrayList!!, this)
+                                        layoutManayer = LinearLayoutManager(this)
+
+                                        reciclerUser!!.layoutManager = layoutManayer
+                                        reciclerUser!!.adapter = userAdater
+                                    }
+
+                                    userAdater!!.notifyDataSetChanged()
+
+                                }catch (e: JSONException) {
+                                    e.printStackTrace()
+                                }
+                            },
+                            Response.ErrorListener {  })
+
+        volleyRequest!!.add(jsonArrayReq)
     }
 }
---
+++ app/src/main/java/com/gaelectronica/infotrack_prueba/adapter/UserListAdapter.kt
@@ -0,0 +1,43 @@
+package com.gaelectronica.infotrack_prueba.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.gaelectronica.infotrack_prueba.R
+import com.gaelectronica.infotrack_prueba.model.User
+
+class UserListAdapter (private val list: ArrayList<User>, private val context: Context):
+    RecyclerView.Adapter<UserListAdapter.ViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context)
+            .inflate(R.layout.item_user, parent, false)
+
+        return  ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        if(holder != null){
+            holder.bindView(list[position])
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return  list.size
+    }
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        var name = itemView.findViewById<TextView>(R.id.txtName)
+        var username = itemView.findViewById<TextView>(R.id.txtUserName)
+        var email = itemView.findViewById<TextView>(R.id.txtEmail)
+
+        fun bindView(user:User) {
+            name.text = user.name
+            username.text = user.username
+            email.text = user.email
+        }
+    }
+}
